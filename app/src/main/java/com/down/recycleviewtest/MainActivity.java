5710e7d93f5c60e8b55afd4995d0596d33b068c1
jituumakanta <jitu.umakanta@gmail.com>
2017-10-09 10:25:36 +0530

    recycleview with adding progressbar at bottom

--- app/src/main/java/com/down/recycleviewtest/MainActivity.java
+++ app/src/main/java/com/down/recycleviewtest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.down.recycleviewtest;
 
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.provider.DocumentsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,10 +17,13 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 import java.util.LinkedList;
 
+import static java.lang.Thread.sleep;
+
 public class MainActivity extends AppCompatActivity {
 
     LinkedList<bean> linkedList;
     MyAdapter myAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,49 +31,144 @@ public class MainActivity extends AppCompatActivity {
         linkedList = new LinkedList<>();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycleview);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        myAdapter = new MyAdapter(this, linkedList);
+        myAdapter = new MyAdapter(this, linkedList, recyclerView);
 
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setHasFixedSize(true);
-       // addDataToLinkedList();
-        download a = new download();
-        a.execute("asxa");
+        linearLayoutManager.findFirstVisibleItemPosition();
+        try {
+            addDataToLinkedList();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        /*download a = new download();
+        a.execute("asxa");*/
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (isLastItemDisplaying(recyclerView)) {
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                //linkedList.remove(linkedList.size() - 1);
+                               // linkedList.remove(linkedList.size() - 3);
+                                linkedList.removeLast();
+                                addDataToLinkedList();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, 500);
+
+
+                }
+            }
+        });
+
+
     }
 
-    public void addDataToLinkedList() {
+    public void addDataToLinkedList() throws InterruptedException {
         for (int i = 0; i < 10; i++) {
             bean bean = new bean();
-            bean.setText1("aaa");
+            String converti=Integer.toString(i);
+            bean.setText1(converti);
             bean.setText2("bbb");
             linkedList.add(bean);
         }
+        myAdapter.notifyDataSetChanged();
+        linkedList.add(null);
+        System.out.println("linkedList.size(): "+linkedList.size());
+
     }
+
+
+    private boolean isLastItemDisplaying(RecyclerView recyclerView) {
+        if (recyclerView.getAdapter().getItemCount() != 0) {
+            int lastVisibleItemPosition = ((LinearLayoutManager) recyclerView.getLayoutManager()).findLastCompletelyVisibleItemPosition();
+            if (lastVisibleItemPosition != RecyclerView.NO_POSITION && lastVisibleItemPosition == recyclerView.getAdapter().getItemCount() - 1)
+                return true;
+        }
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     public void getData() throws IOException {
         Document document = Jsoup.connect("https://blinksowonder.com/category/entertainment/celeberaties/").get();
         Elements elements = document.getElementsByTag("article");
-        for(Element e:elements){
+        for (Element e : elements) {
 
-            String linksrc=null,linkText1=null,linkText2 = null;
+            String linksrc = null, linkText1 = null, linkText2 = null;
 
-            Elements elements1=e.getElementsByTag("img");
-            for(Element element:elements1){
+            Elements elements1 = e.getElementsByTag("img");
+            for (Element element : elements1) {
                 linksrc = element.attr("abs:src");
-                System.out.println("linksrc"+linksrc);
+                System.out.println("linksrc" + linksrc);
             }
 
-            Elements elements2=e.getElementsByClass("entry-header");
-            for(Element element:elements2){
+            Elements elements2 = e.getElementsByClass("entry-header");
+            for (Element element : elements2) {
                 linkText1 = element.text();
-                System.out.println("linkText"+linkText1);
+                System.out.println("linkText" + linkText1);
             }
 
-            Elements elements3=e.getElementsByClass("entry-summary");
-            for(Element element:elements3){
-                 linkText2 = element.text();
-                System.out.println("linkText"+linkText2);
+            Elements elements3 = e.getElementsByClass("entry-summary");
+            for (Element element : elements3) {
+                linkText2 = element.text();
+                System.out.println("linkText" + linkText2);
             }
 
 
@@ -78,12 +178,9 @@ public class MainActivity extends AppCompatActivity {
             bean.setImg(linksrc);
             linkedList.add(bean);
 
-
-
-
         }
-    }
 
+    }
 
     public class download extends AsyncTask<String, Void, String> {
 
--- app/src/main/java/com/down/recycleviewtest/MyAdapter.java
+++ app/src/main/java/com/down/recycleviewtest/MyAdapter.java
@@ -1,95 +1,129 @@
 package com.down.recycleviewtest;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.github.ybq.android.spinkit.style.ThreeBounce;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-import static com.down.recycleviewtest.R.id.textView1;
+import static com.down.recycleviewtest.R.id.progressBar;
+import static java.lang.Thread.sleep;
 
 /**
  * Created by Wrap me 09 on 08-08-2017.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.myView1> {
+public class MyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context context;
     LinkedList<bean> linkedList;
+    RecyclerView recyclerView;
 
-    public MyAdapter(Context context, LinkedList<bean> linkedList) {
+    public MyAdapter(Context context, LinkedList<bean> linkedList, RecyclerView recyclerView) {
         this.context = context;
         this.linkedList = linkedList;
+        this.recyclerView = recyclerView;
+
     }
 
     @Override
-    public myView1 onCreateViewHolder(ViewGroup parent, int viewType) {
-       // View view1=View.inflate(context,R.layout.recycleview_items, parent);
-       // myView1 myView1=;
-      //  myView2 myView2=new myView2();
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.recycleview_items, parent, false);
-        return new myView1(itemView);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = null;
+        View v;
+        // Toast.makeText(context, "viewType: " + viewType, Toast.LENGTH_LONG).show();
+        System.out.println("viewType: " + viewType);
+        if (viewType == 0) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_items1, parent, false);
+            viewHolder = new myView1(v);
+        } else if (viewType == 1) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleview_items2, parent, false);
+            viewHolder = new myView2(v);
+        }
+        return viewHolder;
     }
 
 
     @Override
-    public void onBindViewHolder(MyAdapter.myView1 holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
 
-        holder.textView1.setText(linkedList.get(position).getText1());
-        holder.textView2.setText(linkedList.get(position).getText2());
-        Glide.with(context)
-                .load(linkedList.get(position).getImg())
-                .override(200,100)
-                .into(holder.imageView);
+        if (holder instanceof myView1) {
+            bean bean = linkedList.get(position);
+            ((myView1) holder).textView1.setText(bean.getText1());
+            ((myView1) holder).textView2.setText(bean.getText2());
+            Glide.with(context)
+                    .load(linkedList.get(position).getImg())
+                    .override(200, 100)
+                    .into(((myView1) holder).imageView);
+        } else if (holder instanceof myView2) {
+
+
+            Thread myThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } finally {
+
+
+                    }
+                }
+            };
+            myThread.start();
+        }
     }
 
-    /*@Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
-holder.textView1.
-
-    }*/
 
     @Override
     public int getItemCount() {
-        return linkedList.size();
+        return linkedList == null ? 0 : linkedList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+
+        return linkedList.get(position) == null ? 1 : 0;
+
     }
 
 
+    public class myView1 extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public TextView textView1, textView2;
 
-public class myView1 extends RecyclerView.ViewHolder{
-    ImageView imageView;
-    public TextView textView1,textView2;
-    public myView1(View itemView) {
-        super(itemView);
-        imageView=(ImageView)itemView.findViewById(R.id.imageView);
-        textView1=(TextView)itemView.findViewById(R.id.textView1);
-        textView2=(TextView)itemView.findViewById(R.id.textView2);
-    }
-
-
-}
-
-    public class myView2 extends RecyclerView.ViewHolder{
-
-        public myView2(View itemView) {
+        public myView1(View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.imageView);
+            textView1 = (TextView) itemView.findViewById(R.id.textView1);
+            textView2 = (TextView) itemView.findViewById(R.id.textView2);
         }
 
 
     }
 
+    public class myView2 extends RecyclerView.ViewHolder {
+        public ProgressBar progressBar;
 
+        public myView2(View itemView) {
+            super(itemView);
+            progressBar = (ProgressBar) itemView.findViewById(R.id.progressBar);
+        }
 
-
-
-
+    }
 
 
 }
